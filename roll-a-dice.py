7c438ed4c14766d69fe5cdcae5f07d61008a3e99
PeterJM-W <105742420+PeterJM-W@users.noreply.github.com>
2023-09-19 19:34:11 +0100

    Add files via upload

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -1,7 +1,20 @@
 import random
    
-option = "y"
-while option == 'y':
+rolls = 0
+score = 0
+option = input("Do you want to roll a dice? (y/n): ")
+while option != 'y':
+    if option =='n':
+        print("Wrong answer.")
+        print("Try again. :)")
+    
+    if option != 'y' and option !='n':
+        print("Invalid input")
+        print("Please use y (for yes) or n (for no)")
+        
+    option = input("Do you want to roll a dice? (y/n): ")
+
+while option == 'y':   
     n = random.randint(1, 6)
     if n==1:
         print("[-----]")
@@ -39,5 +52,21 @@ while option == 'y':
         print("[     ]")
         print("[ 000 ]")
         print("[-----]")
-
-    option = input("Do you want to roll a dice? (y/n): ")
+    
+    rolls += 6
+    score += int(n)
+    option = input("Do you want to roll again? (y/n): ")
+    while option != 'y' and option != 'n':
+        input("Please use y (for yes) or n (for no): ")
+    
+    if option == 'n':
+        print("Your score was", score, "/", rolls)
+        if (rolls == score) and (rolls > 6):
+            print("Congratulations! You won! You have great luck!")
+        elif rolls == score:
+            print("You did amazing but take a chance!")
+        elif ((rolls) / 2) < score:
+            print("Well done! You've done quite well")
+        else:
+            print("Good try. Better luck next time...")
+    
